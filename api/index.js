4fb7806df8e037ffc7e6186f799813182d1f47f8
Eya Gargah <eyagargah@hotmail.com>
2022-11-18 14:34:19 +0100

    Update index.js
    setup server

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+const  db = mongoose.connection
+db.on('error', (error)=> console.error(error))
+db.once('open', ()=> console.log('Connected to Database'))
+app.listen(3000, () => 
     console.log(`Server Started at ${3000}`)
-})
+)
