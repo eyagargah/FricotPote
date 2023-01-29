2f105a62775ec0113fd88fc12ef1dcdd381ad807
Eya Gargah <eyagargah@hotmail.com>
2023-01-29 22:55:02 +0100

    Update top bar component

--- frontend/src/app/pages/top-bar/top-bar.component.ts
+++ frontend/src/app/pages/top-bar/top-bar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit {
+  @Input() authToken: any;
 
+  minimal = true;
+  src = '';
+  constructor() {}
+  ngOnInit(): void {
+    let navBtn = document.querySelector('.nav-button')as HTMLButtonElement
+
+    if (this.authToken && this.minimal){
+      navBtn.style.visibility='hidden'
+    }
+    else {
+      navBtn.style.visibility="visible"
+    }
+    if (!this.minimal) {
+      this.src = '../../../assets/tinder_logo_white.png';
+    } else {
+      this.src = '../../../assets/color-logo-tinder.png';
+    }
+  }
 }
