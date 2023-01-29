6cefae85c79651bf8b501de049121df715b16deb
Eya Gargah <eyagargah@hotmail.com>
2023-01-29 22:55:39 +0100

    Create home component

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  authToken = false
+  handleClick(){
+    console.log('hello from home button')
+  }
+}
