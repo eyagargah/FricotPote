86f0f87f27250ed67a0b8f6ea7a955795c2de144
Eya Gargah <eyagargah@hotmail.com>
2023-02-01 00:03:59 +0100

    Update home.component.ts

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { AuthModalComponent } from '../auth-modal/auth-modal.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
   authToken = false
-  handleClick(){
-    console.log('hello from home button')
-  }
+  fileNameDialogRef: MatDialogRef<AuthModalComponent> | undefined;
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    this.fileNameDialogRef = this.dialog.open(AuthModalComponent);
+   
+  } 
 }
+
