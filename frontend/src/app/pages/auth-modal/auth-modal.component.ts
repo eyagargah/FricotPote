86463286955094be079e935f772ddff7ec23b4c4
Eya Gargah <eyagargah@hotmail.com>
2023-02-07 22:24:56 +0100

    Update auth-modal.component.ts

--- frontend/src/app/pages/auth-modal/auth-modal.component.ts
+++ frontend/src/app/pages/auth-modal/auth-modal.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject} from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
-  styleUrls: ['./auth-modal.component.scss']
+  styleUrls: ['./auth-modal.component.scss'],
 })
 export class AuthModalComponent {
-  
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {title: string}) { }
+
+  onSubmit= (e: { preventDefault: () => void; })=> {
+    e.preventDefault()
+    console.log('onSubmit')
+  }
+  setEmail= (e: { preventDefault: () => void; })=> {
+    e.preventDefault()
+    console.log('setEmail')
+  }
 }
