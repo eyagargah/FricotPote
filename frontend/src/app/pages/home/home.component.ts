f7498ad95db85bec00bf04fabfc75568332c7dc8
Eya Gargah <eyagargah@hotmail.com>
2023-02-07 22:24:59 +0100

    Update home.component.ts

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
-import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AuthModalComponent } from '../auth-modal/auth-modal.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  authToken = false
+  
+  authToken = false;
   fileNameDialogRef: MatDialogRef<AuthModalComponent> | undefined;
 
   constructor(private dialog: MatDialog) {}
 
   openDialog() {
-    this.fileNameDialogRef = this.dialog.open(AuthModalComponent);
-   
-  } 
+    this.fileNameDialogRef = this.dialog.open(AuthModalComponent  , {
+      data: { title: 'Create Account' },
+    })
+  }
 }
-
