794faaa965006e9733e5c635b034d9f04c2e8b59
Eya Gargah <eyagargah@hotmail.com>
2023-02-14 23:15:16 +0100

    Update onboarding.component.ts

--- frontend/src/app/pages/onboarding/onboarding.component.ts
+++ frontend/src/app/pages/onboarding/onboarding.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./onboarding.component.scss'],
 })
 export class OnboardingComponent {
+  formData = {
+    user_id:"",
+    first_name: "",
+    dob_day:"",
+    dob_month:"",
+    dob_year:"",
+    show_gender:false,
+    gender_identity:"man",
+    gender_interest:"woman",
+    email:"",
+    url:"",
+    about:"",
+    matches: []
 
+  }
+
+  handleChange(event:any){
+    const value = event.target.value
+    const name = event.target.name
+    console.log('value: '+ value)
+    console.log('name: '+ name)
+  }
+
+  handleSubmit(){
+
+  }
 }
