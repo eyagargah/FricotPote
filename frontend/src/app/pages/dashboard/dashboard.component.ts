c267adfad17cdc2d557507665805fd999afb7290
Eya Gargah <eyagargah@hotmail.com>
2023-02-17 23:43:29 +0100

    Update dashboard.component.ts

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
 
+  
+  dataDb = [
+    {
+      name: 'Elon Musk',
+      url: '../assets/users/elon.jpg',
+    },
+    {
+      name: 'Oprah Winfrey',
+      url: '../assets/users/oprah.jpg',
+    },
+    {
+      name: 'Adam Sandler',
+      url: '../assets/users/adam.jpg',
+    },
+    {
+      name: 'Kevin James',
+      url: '../assets/users/kevin.jpg',
+    },
+    {
+      name: 'Drew Barrymore',
+      url: '../assets/users/drew.jpg',
+    },
+  ];
+
+  
 }
