12e798835752f1276ff3a5f94d8c06e3aa6c43ca
Eya Gargah <eyagargah@hotmail.com>
2023-02-20 22:52:17 +0100

    Update card.component.ts

--- frontend/src/app/pages/card/card.component.ts
+++ frontend/src/app/pages/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-@Input() dataDb: any;
+
 }
