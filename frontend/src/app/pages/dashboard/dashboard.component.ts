8dbd7e3e26113d328de25fdc1b9be4ff27cdce1b
Eya Gargah <eyagargah@hotmail.com>
2023-02-20 22:52:24 +0100

    Update dashboard.component.ts

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,36 +1,61 @@
+import { animate, keyframes, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
-
+import * as kf from './keyframes';
+import data from './users.json';
+import { User } from './user';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('cardAnimator', [
+      transition('* => swiperight', animate(550, keyframes(kf.swiperight))),
+      transition('* => swipeleft', animate(550, keyframes(kf.swipeleft)))
+    ])]
 })
+
 export class DashboardComponent {
+  animationState: string | undefined;
+  parentSubject:Subject<string> = new Subject();
+  public users: User[] = data;
+  public index = 0;
+  constructor() {
 
-  
-  dataDb = [
-    {
-      name: 'Elon Musk',
-      url: '../assets/users/elon.jpg',
-    },
-    {
-      name: 'Oprah Winfrey',
-      url: '../assets/users/oprah.jpg',
-    },
-    {
-      name: 'Adam Sandler',
-      url: '../assets/users/adam.jpg',
-    },
-    {
-      name: 'Kevin James',
-      url: '../assets/users/kevin.jpg',
-    },
-    {
-      name: 'Drew Barrymore',
-      url: '../assets/users/drew.jpg',
-    },
-  ];
+  }
 
-  
+  sendMsg(){
+    console.log('send msg is working')
+  }
+ cardAnimation(value : string) {
+    this.parentSubject.next(value);
+  }
+  ngOnInit() {
+    this.parentSubject?.subscribe(event => {
+      this.startAnimation(event)
+    });
+  }
+
+  sleep(ms: number){
+    return new Promise(res => setTimeout(res , ms))
+  }
+  async startAnimation(state: any) {
+    if (!this.animationState) {
+      this.animationState = state;
+      await this.sleep(500)
+      this.index+=1;
+      
+
+    }
+  }
+
+  resetAnimationState(state: any) {
+    this.animationState = '';
+  }
+
+
+  ngOnDestroy() {
+    this.parentSubject?.unsubscribe();
+  }
+
 }
