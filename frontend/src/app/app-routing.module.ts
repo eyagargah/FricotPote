67711ceb83b2e9b280174debb06b972261eb7630
Eya Gargah <eyagargah@hotmail.com>
2023-02-21 22:35:39 +0100

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OnboardingComponent } from './pages/onboarding/onboarding.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
