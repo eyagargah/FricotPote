a07c831867c51a6ec70a65b569491bd51bee6378
Eya Gargah <eyagargah@hotmail.com>
2023-04-16 00:28:28 +0100

    Create chat display component

---
+++ frontend/src/app/pages/chat-display/chat-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-display',
+  templateUrl: './chat-display.component.html',
+  styleUrls: ['./chat-display.component.scss']
+})
+export class ChatDisplayComponent {
+
+}
