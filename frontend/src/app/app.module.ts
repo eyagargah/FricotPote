3beb7ac10be7bd5b33979e35cafd71286ab303d7
Eya Gargah <eyagargah@hotmail.com>
2023-04-16 00:13:46 +0100

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/d
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ChatContainerComponent } from './pages/chat-container/chat-container.component';
 import { ChatHeaderComponent } from './pages/chat-header/chat-header.component';
+import { MatchesComponent } from './pages/matches/matches.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +26,8 @@ import { ChatHeaderComponent } from './pages/chat-header/chat-header.component';
     AuthModalComponent,
     CardComponent,
     ChatContainerComponent,
-    ChatHeaderComponent
+    ChatHeaderComponent,
+    MatchesComponent
   ],
   imports: [BrowserModule, AppRoutingModule, MatIconModule, MatDialogModule,BrowserAnimationsModule,HammerModule],
   entryComponents: [AuthModalComponent],
