b37a50bc804349ff87880ca3dfd6e82e42bb2efb
Eya Gargah <eyagargah@hotmail.com>
2023-04-18 23:49:15 +0100

    Update chat-header.component.ts

--- frontend/src/app/pages/chat-header/chat-header.component.ts
+++ frontend/src/app/pages/chat-header/chat-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat-header',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-header.component.scss']
 })
 export class ChatHeaderComponent {
-
+  constructor(private router:Router){}
+  logout(){
+    this.router.navigateByUrl('home')
+  }
 }
