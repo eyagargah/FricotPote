1ec42aebd70959049057ef5213d9a896f725ba32
Eya Gargah <eyagargah@hotmail.com>
2023-04-21 23:38:35 +0100

    Update index.js
    Add sign up method

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-  });
+const jwt = require('jsonwebtoken')
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const { MongoClient } = require("mongodb");
 const uri =
@@ -15,8 +20,44 @@ app.get("/", (req, res) => {
   res.json("hello to my app");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const client = new MongoClient(uri);
+  const { email, body } = req.body;
+
+  const generatedUserId = uuidv4();
+  const hashed_password = await bcrypt.hadh(password, 10);
+
+  try {
+    client.connect();
+    const db = client.db("app-data");
+    const users = db.collection("users");
+
+    const existingUser = users.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).send("User already exists.please sign in");
+    }
+
+    const sanitizedEmail = email.toLowerCase();
+
+    const data = {
+      user_id: generatedUserId,
+      email: sanitizedEmail,
+
+      hashed_password: hashed_password,
+    };
+
+   const insertedUser =  await users.insertOne(data)
+
+   const token = jwt.sign(insertedUser, sanitizedEmail, {
+    //expires in 24 hrs
+    expiresIn: 60 * 24,
+   })
+
+   res.status(201).json( { token , userId: generatedUserId ,email: sanitizedEmail})
+  } catch(err){
+    console.log(err)
+  }
 });
 
 //get users
