cfba7aa4713bccf0aec18f8714986be10d4d5edb
Eya Gargah <eyagargah@hotmail.com>
2023-04-21 23:00:09 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,30 +1,37 @@
-const PORT = 8000
-const express = require('express')
-const app = express()
-const { MongoClient } = require('mongodb')
-const uri = 'mongodb+srv://eyagargah:mypassword@cluster0.s98m2ta.mongodb.net/test'
-app.get('/', (req, res) =>  {
-    res.json('hello to my app')
-})
+const express = require("express");
+const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
 
-app.post('/signup', (req, res) =>  {
-    const  client = new MongoClient(uri)
-})
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://eyagargah:mypassword@cluster0.s98m2ta.mongodb.net/test";
+
+app.get("/", (req, res) => {
+  res.json("hello to my app");
+});
+
+app.post("/signup", (req, res) => {
+  const client = new MongoClient(uri);
+});
 
 //get users
-app.get('/users', async(req , res)=> {
-    const  client = new MongoClient(uri)
+app.get("/users", async (req, res) => {
+  const client = new MongoClient(uri);
 
-    try {
-        await client.connect()
-        const db = client.db('app-data')
-        const users = db.collection('users')
-        const returnedUsers = await users.find().toArray()
-        res.send(returnedUsers)
-    }
-    finally {
-        await client.close()
-    }
-})
-app.listen(PORT , ()=> console.log('Server running on PORT ' + PORT))
+  try {
+    await client.connect();
+    const db = client.db("app-data");
+    const users = db.collection("users");
+    const returnedUsers = await users.find().toArray();
+    res.send(returnedUsers);
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(8000, () => console.log(`Server Started at ${8000}`));
