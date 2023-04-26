507537b14191331223d5fc3cb6bc317dfc35ba00
Eya Gargah <eyagargah@hotmail.com>
2023-04-26 22:35:01 +0100

    Update chat-header.component.ts
    get user data from db

--- frontend/src/app/pages/chat-header/chat-header.component.ts
+++ frontend/src/app/pages/chat-header/chat-header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import axios from 'axios';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-chat-header',
@@ -7,7 +9,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./chat-header.component.scss']
 })
 export class ChatHeaderComponent {
-  constructor(private router:Router){}
+  constructor(private router:Router, private cookiesservice: CookieService){}
+ 
+
+  userId = this.cookiesservice.get('UserId')
+  user: any
+ ngOnInit(){
+  this.getUser()
+ }
+  getUser= async() =>{
+    try {
+    const response = await axios.get('http://localhost:8000/user', {
+        params: {userId: this.userId}
+      })
+     this.user = response.data
+      console.log("🚀 ~ file: dashboard.component.ts:38 ~ DashboardComponent ~ getUser=async ~ user:", this.user)
+      
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
   logout(){
     this.router.navigateByUrl('home')
   }
