c00e4a1b5b5110deb39b91c002a7759ebd931d6a
Eya Gargah <eyagargah@hotmail.com>
2023-06-06 21:03:03 +0100

    update matches per user

--- server/index.js
+++ server/index.js
@@ -190,4 +190,26 @@ app.get("/gendered-users", async (req, res) => {
   }
 });
 
+// Update User with a match
+app.put('/addmatch', async (req, res) => {
+  const client = new MongoClient(uri)
+  const {userId, matchedUserId} = req.body
+
+  try {
+      await client.connect()
+      const database = client.db('app-data')
+      const users = database.collection('users')
+
+      const query = {user_id: userId}
+      const updateDocument = {
+          $push: {matches: {user_id: matchedUserId}}
+      }
+      const user = await users.updateOne(query, updateDocument)
+      res.send(user)
+  } finally {
+      await client.close()
+  }
+})
+
+
 app.listen(8000, () => console.log(`Server Started at ${8000}`));
