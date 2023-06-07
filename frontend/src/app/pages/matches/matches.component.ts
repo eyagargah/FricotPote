686307b3cd40350e1f7c8741150e965be0847893
Eya Gargah <eyagargah@hotmail.com>
2023-06-07 21:50:30 +0100

    Update matches.component.ts

--- frontend/src/app/pages/matches/matches.component.ts
+++ frontend/src/app/pages/matches/matches.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-matches',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./matches.component.scss']
 })
 export class MatchesComponent {
-
+  constructor(private cookiesService:CookieService){}
+  userId = this.cookiesService.get('UserId');
+  matchedUsers: any
+  getMatchedUsers= async()=> {
+    try {
+      const response = await axios.get('http://localhost:8000/user', {
+        params: { userId: this.userId },
+      });
+      this.matchedUsers = response.data;
+    } 
+    catch(err){
+      console.log(err)
+    }
+  
+  }
 }
