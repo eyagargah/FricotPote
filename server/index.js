1416b76689c40473cd474501a2ff96914fde37c7
Eya Gargah <eyagargah@hotmail.com>
2023-06-12 22:06:18 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -155,31 +155,19 @@ app.post("/login", async (req, res) => {
   }
 });
 
-//get all users
-app.get("/users", async (req, res) => {
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    const db = client.db("app-data");
-    const users = db.collection("users");
-    const returnedUsers = await users.find().toArray();
-    res.send(returnedUsers);
-  } finally {
-    await client.close();
-  }
-});
 
 //get users
 app.get("/users", async (req, res) => {
   const client = new MongoClient(uri);
 
+  const userIds = JSON.parse(req.body.userIds)
+  console.log(userIds)
   try {
     await client.connect();
     const db = client.db("app-data");
     const users = db.collection("users");
-    const returnedUsers = await users.find().toArray();
-    res.send(returnedUsers);
+    
+    
   } finally {
     await client.close();
   }
