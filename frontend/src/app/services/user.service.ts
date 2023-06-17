a8ce8720639779aa80af42cc149dd81ba4dec352
Eya Gargah <eyagargah@hotmail.com>
2023-06-17 23:12:35 +0100

    Update user.service.ts

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+ 
+  selectedUser: any
+  constructor(private cookiesServices: CookieService) { }
+  getCurrentUser(){
+    return this.cookiesServices.get('user_id')
+  }
 
-  constructor() { }
+  
 }
