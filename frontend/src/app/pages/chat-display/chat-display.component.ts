1b3640f3901aa362f15c8b358a8f81351ed38d38
Eya Gargah <eyagargah@hotmail.com>
2023-06-17 23:18:29 +0100

    Update chat-display.component.ts

--- frontend/src/app/pages/chat-display/chat-display.component.ts
+++ frontend/src/app/pages/chat-display/chat-display.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import axios from 'axios';
 import { CookieService } from 'ngx-cookie-service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-chat-display',
@@ -8,10 +9,15 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./chat-display.component.scss'],
 })
 export class ChatDisplayComponent {
-  constructor(private cookiesServices: CookieService){}
+  constructor(private cookiesServices: CookieService, private userService: UserService){}
+  ngOnInit(){
+    this.selectedUser = this.userService.getSelectedUser()
+    this.selectedUserId = this.selectedUser.user_id
+  }
   messages: any;
   userId = this.cookiesServices.get('user_id')
   selectedUserId: any
+  selectedUser: any
   getMessages = async () => {
     try {
       const response = await axios.get('http://localhost:8000/messages' , {params :{ user_id : this.userId , correspondingUserId: this.selectedUserId}});
