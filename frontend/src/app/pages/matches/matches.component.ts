fa2d5880d3037e1fe6edf64680f2c59ff4b716f4
Eya Gargah <eyagargah@hotmail.com>
2023-06-23 22:59:13 +0100

    Update matches.component.ts

--- frontend/src/app/pages/matches/matches.component.ts
+++ frontend/src/app/pages/matches/matches.component.ts
@@ -7,39 +7,40 @@ import { ChatDisplayComponent } from '../chat-display/chat-display.component';
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
-  styleUrls: ['./matches.component.scss']
+  styleUrls: ['./matches.component.scss'],
 })
 export class MatchesComponent {
-  constructor(private cookiesService: CookieService, private userService:UserService){}
-  userId= this.cookiesService.get('userId')
-  @Input() matches: any
-  @Input() showMatches: any
-
-  matchedUserIds : any
-  matchesData: any
-  selectedUser: any
-  displayChat: any
-  getMatches = async(userId: any)=> {
+  constructor(
+    private cookiesService: CookieService,
+    private userService: UserService
+  ) {}
+  userId = this.cookiesService.get('userId');
+  @Input() matches: any;
+  @Input() showMatches: any;
+  @Input() user: any;
+  matchedUserIds: any;
+  matchesData: any;
+  selectedUser: any;
+  displayChat: any;
+  getMatches = async (userId: any) => {
     try {
       const response = await axios.get('http://localhost:8000/users', {
-        params: { userIds: this.matchedUserIds}
-      })
-      this.matchesData = response.data
-    }catch(err){
-      console.log(err)
+        params: { userIds: this.matchedUserIds },
+      });
+      this.matchesData = response.data;
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
-
-  ngOnInit(){
+  ngOnInit() {
     //this.matchedUserIds = this.matches.map( (m: { user: { user_id: any; }; }) => m.user.user_id)
     //this.getMatches(this.userId)
   }
- 
-  getUser(e:any){
-    this.selectedUser= e
-    console.log(this.selectedUser)
-    this.userService.setSelectedUser(this.selectedUser)
-    
+
+  getUser(e: any) {
+    this.selectedUser = e;
+    console.log(this.selectedUser);
+    this.userService.setSelectedUser(this.selectedUser);
   }
 }
