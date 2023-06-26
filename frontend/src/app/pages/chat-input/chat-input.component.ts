f9d4deb38ceedbfcaa76e18d4065c8a8111d6700
Eya Gargah <eyagargah@hotmail.com>
2023-06-26 22:33:37 +0100

    Update chat-input.component.ts
    send message to selected user

--- frontend/src/app/pages/chat-input/chat-input.component.ts
+++ frontend/src/app/pages/chat-input/chat-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import axios from 'axios';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-chat-input',
@@ -8,23 +9,23 @@ import axios from 'axios';
 })
 export class ChatInputComponent {
   @Input() currentUser: any;
-  sendMsg = async (e: any) => {
+  constructor(private userService: UserService){}
+  
+  sendMsg = async (e:any) => {
     const chatInput = document.querySelector('.msg') as HTMLTextAreaElement;
+    let selectedUserId= this.userService.getSelectedUserId
     const message = {
-      timestamp: new Date().toISOString(),
-      from_userId: this.currentUser,
-      to_userId: clickedUserId,
-      message: chatInput.value
-  }
-    console.log(chatInput.value);
-    if (chatInput && e.keyCode === 13) {
-      try {
-        const response = await axios.post('http://localhost:8000/message', {
-          message: message,
-        });
-      } catch (err) {
-        console.log(err);
-      }
+        timestamp: new Date().toISOString(),
+        from_userId: this.currentUser,
+        to_userId: selectedUserId,
+        message: chatInput.value
     }
-  };
+
+    try {
+        await axios.post('http://localhost:8000/message', { message:message })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 }
