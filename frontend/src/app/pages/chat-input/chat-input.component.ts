82e4c5f739ecce5d9798c4d2ec3b035adffdafe0
Eya Gargah <eyagargah@hotmail.com>
2023-06-26 22:17:46 +0100

    Update chat-input.component.ts

--- frontend/src/app/pages/chat-input/chat-input.component.ts
+++ frontend/src/app/pages/chat-input/chat-input.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-chat-input',
   templateUrl: './chat-input.component.html',
-  styleUrls: ['./chat-input.component.scss']
+  styleUrls: ['./chat-input.component.scss'],
 })
 export class ChatInputComponent {
-
-  sendMsg(e:any){
-    const chatInput= document.querySelector('.msg') as HTMLTextAreaElement
-    console.log(chatInput.value)
+  @Input() currentUser: any;
+  sendMsg = async (e: any) => {
+    const chatInput = document.querySelector('.msg') as HTMLTextAreaElement;
+    const message = {
+      timestamp: new Date().toISOString(),
+      from_userId: this.currentUser,
+      to_userId: clickedUserId,
+      message: textArea
   }
+    console.log(chatInput.value);
+    if (chatInput && e.keyCode === 13) {
+      try {
+        const response = await axios.post('http://localhost:8000/message', {
+          message: message,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 }
