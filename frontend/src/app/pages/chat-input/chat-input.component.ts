8733a1c725df077c8ee7b0ff686fde3ff9b34a64
Eya Gargah <eyagargah@hotmail.com>
2023-06-27 21:53:16 +0100

    Update chat-input.component.ts

--- frontend/src/app/pages/chat-input/chat-input.component.ts
+++ frontend/src/app/pages/chat-input/chat-input.component.ts
@@ -9,24 +9,26 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ChatInputComponent {
   @Input() currentUser: any;
-  constructor(private userService: UserService){}
-  
-  sendMsg = async (e:any) => {
-    const chatInput = document.querySelector('.msg') as HTMLTextAreaElement;
-    let selectedUserId= this.userService.getSelectedUserId
-    let currentUserId = this.currentUser.user_id
-    const message = {
-        timestamp: new Date().toISOString(),
-        from_userId: currentUserId,
-        to_userId: selectedUserId,
-        message: chatInput.value
-    }
+  constructor(private userService: UserService) {}
 
-    try {
-        await axios.post('http://localhost:8000/message', { message:message })
-        
-    } catch (error) {
-        console.log(error)
+  sendMsg = async (e: any) => {
+    const chatInput = document.querySelector('.msg') as HTMLTextAreaElement;
+    const selectedUserId = this.userService.getSelectedUserId();
+
+    let currentUserId = this.currentUser.user_id;
+    if (selectedUserId) {
+      const message = {
+        timestamp: new Date().toISOString(),
+        to_userId: selectedUserId,
+        from_userId: currentUserId,
+        message: chatInput.value,
+      };
+
+      try {
+        await axios.post('http://localhost:8000/message', { message: message });
+      } catch (error) {
+        console.log(error);
+      }
     }
-}
+  };
 }
