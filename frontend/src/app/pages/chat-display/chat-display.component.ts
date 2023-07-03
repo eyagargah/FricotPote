64cd380724273af8fb14ca5b9ded2231d1a0c83b
Eya Gargah <eyagargah@hotmail.com>
2023-07-03 16:52:57 +0100

    Update chat-display.component.ts

--- frontend/src/app/pages/chat-display/chat-display.component.ts
+++ frontend/src/app/pages/chat-display/chat-display.component.ts
@@ -9,30 +9,46 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./chat-display.component.scss'],
 })
 export class ChatDisplayComponent {
-  constructor(private cookiesServices: CookieService, private userService: UserService){}
-  @Input() currentUser:any
+  constructor(
+    private cookiesServices: CookieService,
+    private userService: UserService
+  ) {}
+  @Input() currentUser: any;
   userMessages: any;
-  senderId : any
-  recepientId: any
-  selectedUser: any
-  clickedUserMessages: any
-  messages : any;
+  senderId: any;
+  recepientId: any;
+  selectedUser: any;
+  clickedUserMessages: any;
+  messages: any;
 
-  ngOnInit(){
-    this.senderId =  this.cookiesServices.get('UserId')
-    this.selectedUser = this.userService.getSelectedUser()
-    this.recepientId = this.selectedUser.user_id
-    this.userMessages = this.getMessages(this.senderId, this.recepientId)
-    this.clickedUserMessages=this.getMessages(this.recepientId, this.senderId)
-   
+  ngOnInit() {
+    this.senderId = this.cookiesServices.get('UserId');
+    this.selectedUser = this.userService.getSelectedUser();
+    this.recepientId = this.selectedUser.user_id;
+    this.userMessages = this.getMessages(this.senderId, this.recepientId);
+    this.clickedUserMessages = this.getMessages(
+      this.recepientId,
+      this.senderId
+    );
 
-    
+    if(this.userMessages){
+    setTimeout(this.displayMsg, 2000)
+    }
    
   }
- 
+
+  displayMsg(){
+    for (let i = 0; i < Object.keys(this.userMessages).length; i++) {
+      console.log(
+        Object.keys(this.userMessages)[i]
+      );
+    }
+  }
   getMessages = async (senderId: any, recepientId: any) => {
     try {
-      const response = await axios.get('http://localhost:8000/messages' , {params :{ userId : senderId , correspondingUserId: recepientId}});
+      const response = await axios.get('http://localhost:8000/messages', {
+        params: { userId: senderId, correspondingUserId: recepientId },
+      });
       return response.data;
     } catch (err) {
       console.log(err);
