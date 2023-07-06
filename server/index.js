bd80e4a994cadaefd2115b9847649c466e7cea41
Eya Gargah <eyagargah@hotmail.com>
2023-07-06 22:41:43 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -238,7 +238,10 @@ app.get('/messages', async (req, res) => {
       const query = {
           from_userId: userId, to_userId: correspondingUserId
       }
+      const query2 = { from_userId: correspondingUserId , to_userId: userId}
       const foundMessages = await messages.find(query).toArray()
+      const foundMessages2 = await messages.find(query2).toArray()
+      foundMessages.push(foundMessages2)
       res.send(foundMessages)
   } finally {
       await client.close()
