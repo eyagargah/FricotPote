7233a431bdbbb98f03b530435698801f946609f8
Eya Gargah <eyagargah@hotmail.com>
2023-07-06 22:41:40 +0100

    Update chat-input.component.ts

--- frontend/src/app/pages/chat-input/chat-input.component.ts
+++ frontend/src/app/pages/chat-input/chat-input.component.ts
@@ -51,6 +51,7 @@ export class ChatInputComponent {
       try {
         await axios.post('http://localhost:8000/message', { message: message });
         chatInput.value=""
+        
       } catch (error) {
         console.log(error);
       }
