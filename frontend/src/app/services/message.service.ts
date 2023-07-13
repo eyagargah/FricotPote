637927e74b0dae1b04b04d2c7a01e8e3d86c5adc
Eya Gargah <eyagargah@hotmail.com>
2023-07-13 18:50:30 +0100

    Update message.service.ts

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class MessageService {
 
   constructor() { }
+  getMessage(){}
+
+  sendMsg(){}
+
+  formatMsg(message : any){
+    let formattedMsg = {
+      timestamp:new Date(message.timestamp) ,
+      url : message.from_user.url,
+      message: message.message,
+    }
+    return formattedMsg
+  }
+
+  sortMessages(messages:any){
+     messages.sort(function(a:any,b:any){
+      return a.timestamp - b.timestamp
+    })
+  }
+
+  filterMessages(messages:any){
+    return messages.filter((el: undefined) => el != undefined);
+  }
 }
