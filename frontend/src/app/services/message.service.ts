dd2930645de65b931e9ebc18e1e0d613d6393885
Eya Gargah <eyagargah@hotmail.com>
2023-07-14 22:56:42 +0100

    Update message.service.ts

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -21,7 +21,7 @@ export class MessageService {
 
   sortMessages(messages:any){
      messages.sort(function(a:any,b:any){
-      return +new Date(a.timestamp) - +new Date(b.timestamp)
+      return a.timestamp - b.timestamp
     })
   }
 
