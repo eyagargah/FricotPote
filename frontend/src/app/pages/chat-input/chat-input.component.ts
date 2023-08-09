7b83a76edd53801aa88b3b035f700ed89f030172
Eya Gargah <eyagargah@hotmail.com>
2023-08-09 23:18:52 +0100

    Update chat-input.component.ts

--- frontend/src/app/pages/chat-input/chat-input.component.ts
+++ frontend/src/app/pages/chat-input/chat-input.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+} from '@angular/core';
 import axios from 'axios';
 import { CookieService } from 'ngx-cookie-service';
 import { MessageService } from 'src/app/services/message.service';
@@ -8,7 +13,7 @@ import { UserService } from 'src/app/services/user.service';
   selector: 'app-chat-input',
   templateUrl: './chat-input.component.html',
   styleUrls: ['./chat-input.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatInputComponent {
   constructor(
@@ -30,7 +35,9 @@ export class ChatInputComponent {
     this.selectedUser = this.userService.getSelectedUser();
     this.senderId = this.cookiesServices.get('UserId');
   }
-
+  update() {
+    this.cd.detectChanges();
+  }
   sendMsg = async (e: any) => {
     const chatInput = document.querySelector('.msg') as HTMLTextAreaElement;
     if (this.selectedUser && chatInput.value !== '') {
@@ -47,34 +54,38 @@ export class ChatInputComponent {
         await axios.post('http://localhost:8000/message', { message: message });
         chatInput.value = '';
 
-        this.messageService.getMessages(this.senderId, this.recepientId).then((data) => {
-          this.userMessages = data;
-          for (let i = 0; i <= this.userMessages.length; i++) {
-            this.messages.push(this.messageService.formatMsg((this.userMessages[i])));
-          }
-          this.messages = this.messageService.filterMessages(this.messages)
-          this.messageService.sortMessages(this.messages)
-          console.log(this.messages + "messages from sender")
-        });
-    
-    
-        this.messageService.sortMessages(this.messages)
-        this.messageService.getMessages(this.recepientId, this.senderId).then((data) => {
-          this.clickedUserMessages = data;
-          for (let i = 0; i <= this.clickedUserMessages.length; i++) {
-            this.messages.push(this.messageService.formatMsg((this.clickedUserMessages[i])));
-          }
-          this.messages = this.messageService.filterMessages(this.messages)
-          this.messageService.sortMessages(this.messages)
-          
-          for (let i=0 ; i<=this.messages.length ; i++){
-            console.log(this.messages[i].timestamp)
-          }
-          
-        });
-        this.messageService.sortMessages(this.messages)
-    
-        
+        this.messageService
+          .getMessages(this.senderId, this.recepientId)
+          .then((data) => {
+            this.userMessages = data;
+            for (let i = 0; i <= this.userMessages.length; i++) {
+              this.messages.push(
+                this.messageService.formatMsg(this.userMessages[i])
+              );
+            }
+            this.messages = this.messageService.filterMessages(this.messages);
+            this.messageService.sortMessages(this.messages);
+            console.log(this.messages + 'messages from sender');
+          });
+
+        this.messageService.sortMessages(this.messages);
+        this.messageService
+          .getMessages(this.recepientId, this.senderId)
+          .then((data) => {
+            this.clickedUserMessages = data;
+            for (let i = 0; i <= this.clickedUserMessages.length; i++) {
+              this.messages.push(
+                this.messageService.formatMsg(this.clickedUserMessages[i])
+              );
+            }
+            this.messages = this.messageService.filterMessages(this.messages);
+            this.messageService.sortMessages(this.messages);
+
+            for (let i = 0; i <= this.messages.length; i++) {
+              console.log(this.messages[i].timestamp);
+            }
+          });
+        this.messageService.sortMessages(this.messages);
       } catch (error) {
         console.log(error);
       }
