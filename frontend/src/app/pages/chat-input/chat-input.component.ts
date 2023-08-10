af5af2f2dd317013eac45e95c0d6f4e7b4cbf130
Eya Gargah <eyagargah@hotmail.com>
2023-08-10 09:24:27 +0100

    Update chat-input.component.ts

--- frontend/src/app/pages/chat-input/chat-input.component.ts
+++ frontend/src/app/pages/chat-input/chat-input.component.ts
@@ -54,38 +54,6 @@ export class ChatInputComponent {
         await axios.post('http://localhost:8000/message', { message: message });
         chatInput.value = '';
 
-        this.messageService
-          .getMessages(this.senderId, this.recepientId)
-          .then((data) => {
-            this.userMessages = data;
-            for (let i = 0; i <= this.userMessages.length; i++) {
-              this.messages.push(
-                this.messageService.formatMsg(this.userMessages[i])
-              );
-            }
-            this.messages = this.messageService.filterMessages(this.messages);
-            this.messageService.sortMessages(this.messages);
-            console.log(this.messages + 'messages from sender');
-          });
-
-        this.messageService.sortMessages(this.messages);
-        this.messageService
-          .getMessages(this.recepientId, this.senderId)
-          .then((data) => {
-            this.clickedUserMessages = data;
-            for (let i = 0; i <= this.clickedUserMessages.length; i++) {
-              this.messages.push(
-                this.messageService.formatMsg(this.clickedUserMessages[i])
-              );
-            }
-            this.messages = this.messageService.filterMessages(this.messages);
-            this.messageService.sortMessages(this.messages);
-
-            for (let i = 0; i <= this.messages.length; i++) {
-              console.log(this.messages[i].timestamp);
-            }
-          });
-        this.messageService.sortMessages(this.messages);
       } catch (error) {
         console.log(error);
       }
