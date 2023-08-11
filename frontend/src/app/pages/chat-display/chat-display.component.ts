148c5308832374a15484b99d982b08e712d83e1c
Eya Gargah <eyagargah@hotmail.com>
2023-08-11 21:39:09 +0100

    Update chat-display.component.ts

--- frontend/src/app/pages/chat-display/chat-display.component.ts
+++ frontend/src/app/pages/chat-display/chat-display.component.ts
@@ -28,33 +28,7 @@ export class ChatDisplayComponent {
     this.selectedUser = this.userService.getSelectedUser();
     this.recepientId = this.selectedUser.user_id;
 
-    this.messageService.getMessages(this.senderId, this.recepientId).then((data) => {
-      this.userMessages = data;
-      for (let i = 0; i <= this.userMessages.length; i++) {
-        this.messages.push(this.messageService.formatMsg((this.userMessages[i])));
-      }
-      this.messages = this.messageService.filterMessages(this.messages)
-      this.messageService.sortMessages(this.messages)
-      console.log(this.messages + "messages from sender")
-    });
-
-
-    this.messageService.sortMessages(this.messages)
-    this.messageService.getMessages(this.recepientId, this.senderId).then((data) => {
-      this.clickedUserMessages = data;
-      for (let i = 0; i <= this.clickedUserMessages.length; i++) {
-        this.messages.push(this.messageService.formatMsg((this.clickedUserMessages[i])));
-      }
-      this.messages = this.messageService.filterMessages(this.messages)
-      this.messageService.sortMessages(this.messages)
-      
-      for (let i=0 ; i<=this.messages.length ; i++){
-        console.log(this.messages[i].timestamp)
-      }
-      
-    });
-    this.messageService.sortMessages(this.messages)
-    this.messages = this.messageService.removeDuplicatesFromMessages(this.messages)
+    
   }
 
  addMsg(message:any){
