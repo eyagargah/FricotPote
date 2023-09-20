41d618ed9d30256d6a2334cc2991ef3ee337511e
Eya Gargah <eyagargah@hotmail.com>
2023-09-20 16:02:43 +0100

    Update message.service.ts

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -8,6 +8,36 @@ export class MessageService {
 
   constructor() { }
   messages: any = []
+  userMessages: any= []
+  senderMessages: any= []
+  getAllMessages = async(user1 : any , user2: any)=> {
+    try {
+      const response1 = await axios.get('http://localhost:8000/messages', {
+        params: { userId: user1, correspondingUserId: user2 },
+      });
+      this.userMessages.push (response1.data);
+      for (let i = 0; i <= this.userMessages.length; i++) {
+        this.messages.push(this.formatMsg((this.userMessages[i])));
+      }
+      this.messages = this.filterMessages(this.userMessages)
+      this.sortMessages(this.messages)
+      
+      const response2 = await axios.get('http://localhost:8000/messages', {
+        params: { userId: user2, correspondingUserId: user1 },
+      });
+      this.senderMessages.push (response2.data);
+      
+      for (let i = 0; i <= this.senderMessages.length; i++) {
+        this.messages.push(this.formatMsg((this.senderMessages[i])));
+      }
+      this.messages = this.filterMessages(this.messages)
+      this.sortMessages(this.messages)
+      
+      return this.messages
+    } catch (err) {
+      console.log(err);
+    }
+  }
   getMessages = async (senderId: any, recepientId: any) => {
     try {
       const response = await axios.get('http://localhost:8000/messages', {
