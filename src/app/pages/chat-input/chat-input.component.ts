ddded4a2c83d4f57c39620fdbe904bb8dd210cbc
Eya Gargah <eyagargah@hotmail.com>
2024-03-12 23:48:34 +0100

    Update chat-input.component.ts

--- src/app/pages/chat-input/chat-input.component.ts
+++ src/app/pages/chat-input/chat-input.component.ts
@@ -50,7 +50,7 @@ export class ChatInputComponent {
         message: chatInput.value,
       };
       try {
-        await axios.post('http://localhost:8000/message', { message: message });
+        await axios.post('https://fricotpote-backend.onrender.com/message', { message: message });
         //get messages sent from this user
         this.messageService.getMessages(this.senderId, this.recepientId).then((data) => {
           this.userMessages = data;
