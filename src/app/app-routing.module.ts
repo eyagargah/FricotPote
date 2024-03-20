5b32b2fa5c65760c1863bc4d443932f3dbb72dad
Eya Gargah <eyagargah@hotmail.com>
2024-03-21 00:15:17 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OnboardingComponent } from './pages/onboarding/onboarding.component';
+import { PreferenceComponent } from './pages/preference/preference.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'home', component: HomeComponent},
   {path:'dashboard', component: DashboardComponent},
-  {path:'onboarding', component: OnboardingComponent}
+  {path:'onboarding', component: OnboardingComponent}, 
+  {path:'preference' , component: PreferenceComponent}
 ];
 
 @NgModule({
