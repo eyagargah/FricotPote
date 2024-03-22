10fafe09f64fb8dcad7648dabdf6868f337f0fc8
Eya Gargah <eyagargah@hotmail.com>
2024-03-22 23:57:41 +0100

    Update location.service.ts

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -7,8 +7,32 @@ export class LocationService {
 
   constructor() { }
 
-  getPlacesNearMe(){
+    getPlacesNearMe(){
 
+  }
+  
+  async getLocation() {
+    const axios = require('axios');
+
+    const options = {
+      method: 'GET',
+      url: 'https://map-places.p.rapidapi.com/queryautocomplete/json',
+      params: {
+        input: 'pizza near Sydney',
+        radius: '50000',
+      },
+      headers: {
+        'X-RapidAPI-Key': '390ad486a1mshad7d5a1836bba4ep1285e6jsn91be75833afa',
+        'X-RapidAPI-Host': 'map-places.p.rapidapi.com',
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   setMapRadius(){
