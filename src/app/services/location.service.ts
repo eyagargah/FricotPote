fb2eb1c6ca51be7baf0930b5aba34061ebfa92a8
Eya Gargah <eyagargah@hotmail.com>
2024-03-22 23:39:25 +0100

    Update location.service.ts

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -7,8 +7,6 @@ export class LocationService {
 
   constructor() { }
 
-  getLocation(){}
-
   getPlacesNearMe(){
 
   }
@@ -18,6 +16,36 @@ export class LocationService {
   }
 
   setSearchLocation(){
-    
+
+  }
+
+  getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            if (position) {
+              console.log(
+                'Latitude: ' +
+                  position.coords.latitude +
+                  'Longitude: ' +
+                  position.coords.longitude
+              );
+              let lat = position.coords.latitude;
+              let lng = position.coords.longitude;
+
+              const location = {
+                lat,
+                lng,
+              };
+              resolve(location);
+            }
+          },
+          (error) => console.log(error)
+        );
+      } else {
+        reject('Geolocation is not supported by this browser.');
+      }
+    });
   }
 }
