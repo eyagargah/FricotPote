5ff285f351413a585cbb7280b928509598ba96ba
Eya Gargah <eyagargah@hotmail.com>
2024-04-24 22:41:28 +0100

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -113,7 +113,6 @@ export class DashboardComponent {
   updateMatchedUsers = async () => {
     for (let i = 0; i < this.likes.length; i++) {
       for (let j = 0; j < this.likes[i].user.likes.length; j++) {
-        console.log(this.currentUser.user_id);
         if (
           this.likes[i].user.likes[j].user.user_id == this.currentUser.user_id
         ) {
