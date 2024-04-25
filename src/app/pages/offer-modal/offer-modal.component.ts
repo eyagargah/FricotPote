059ea187f1b5eadea81082d9728f69e9bc8b0598
Eya Gargah <eyagargah@hotmail.com>
2024-04-25 09:56:03 +0100

    Create offer modal

---
+++ src/app/pages/offer-modal/offer-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-modal',
+  templateUrl: './offer-modal.component.html',
+  styleUrls: ['./offer-modal.component.scss']
+})
+export class OfferModalComponent {
+
+}
