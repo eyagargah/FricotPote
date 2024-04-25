9daed2a7b6ba5dae7520bdd06536f66c01b6b225
Eya Gargah <eyagargah@hotmail.com>
2024-04-25 09:55:51 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OnboardingComponent } from './pages/onboarding/onboarding.component';
 import { PreferenceComponent } from './pages/preference/preference.component';
+import { OfferComponent } from './pages/offer/offer.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'home', component: HomeComponent},
   {path:'dashboard', component: DashboardComponent},
   {path:'onboarding', component: OnboardingComponent}, 
-  {path:'preference' , component: PreferenceComponent}
+  {path:'preference' , component: PreferenceComponent},
+  {path:'offer' , component: OfferComponent}
+
 ];
 
 @NgModule({
