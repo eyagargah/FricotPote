a4099fa867a35c870b1c5a7020bac8ca11c6fe42
Eya Gargah <eyagargah@hotmail.com>
2024-04-26 15:15:20 +0100

    Update offer.component.ts

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offer',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./offer.component.scss']
 })
 export class OfferComponent {
-
+  constructor(private router:Router){}
+  skipOffer(){
+    this.router.navigateByUrl('dashboard')
+  }
 }
