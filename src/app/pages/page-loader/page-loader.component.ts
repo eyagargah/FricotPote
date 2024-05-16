e994871bd91f87668f3582ed065d7863f1cff67c
Eya Gargah <eyagargah@hotmail.com>
2024-05-16 23:17:29 +0100

    Update page-loader.component.ts

--- src/app/pages/page-loader/page-loader.component.ts
+++ src/app/pages/page-loader/page-loader.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { NgxSpinnerModule } from 'ngx-spinner';
 @Component({
   selector: 'app-page-loader',
   templateUrl: './page-loader.component.html',
@@ -10,7 +9,6 @@ export class PageLoaderComponent {
 constructor( private spinner: NgxSpinnerService){}
 openSpinner(){
   this.spinner.show();
-
     setTimeout(() => {
          this.spinner.hide();
     }, 2000);
